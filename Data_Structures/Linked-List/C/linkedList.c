bb1004b7a6ee1b4117976a057e5cc1a5f7709786
Fabio Henrique Farias da Silva <fabioh.farias@outlook.com>
2024-03-25 10:00:43 -0300

    update

---
+++ Data_Structures/Linked-List/C/linkedList.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definição da estrutura do nó
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+
+// Função para criar um novo nó
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->next = NULL;
+    return node;
+}
+
+// Função para ligar os nós
+void connectNode(struct Node* node1, struct Node* node2) {
+    node1->next = node2;
+}
+
+
+// Função para imprimir os elementos da lista encadeada
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    // Criando os nós da lista encadeada
+    struct Node* head = newNode(1);
+    struct Node* second = newNode(2);
+    struct Node* third = newNode(3);
+
+    // Ligando os nós
+    connectNode(head, second);
+    connectNode(second, third);
+
+    // Imprimindo os elementos da lista encadeada
+    printf("Lista Encadeada: ");
+    printList(head);
+
+    return 0;
+}
