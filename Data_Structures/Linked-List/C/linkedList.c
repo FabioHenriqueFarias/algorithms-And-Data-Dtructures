45c03d88c276c9c8a7b3842c241afbe472ab3362
Fabio Henrique Farias da Silva <fabioh.farias@outlook.com>
2024-04-11 09:37:23 -0300

    adding items to the  LinkedList  in C

--- Data_Structures/Linked-List/C/linkedList.c
+++ Data_Structures/Linked-List/C/linkedList.c
@@ -2,29 +2,58 @@
 #include <stdlib.h>
 #include <linkedList.h>
 
-// Definição da estrutura do nó
-struct Node {
-    int data;
-    struct Node* next;
-};
 
 
 // Função para criar um novo nó
-struct Node* newNode(int data) {
-    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+Node* newNode(int data) {
+    Node* node = (Node*)malloc(sizeof(Node));
+    if (node == NULL) {
+        printf("Erro: Não foi possível alocar memória para o novo nó.\n");
+        exit(1);
+    }
     node->data = data;
     node->next = NULL;
     return node;
 }
 
+// Função para criar uma nova lista encadeada
+LinkedList newLinkedList() {
+    LinkedList list;
+    list.head = NULL;
+    return list;
+}
+
+// Função para adicionar um novo nó no final da lista
+void appendNode(LinkedList list, int data) {
+    struct Node* node = newNode(data);
+    if (list.head == NULL) {
+        list.head = node;
+        return;
+    }
+    struct Node* temp = list.head;
+
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = node;
+}
+
+// Função para adicionar um novo nó no início da lista
+void pushNode(LinkedList list, int data) {
+    Node node = newNode(data);
+    Node* temp = list.head;
+    list.head = &node;
+    node.next = temp;
+}
+
 // Função para ligar os nós
-void connectNode(struct Node* node1, struct Node* node2) {
+void connectNode(Node node1, Node node2) {
     node1->next = node2;
 }
 
 // Função para imprimir os elementos da lista encadeada
-void printList(struct Node* head) {
-    struct Node* temp = head;
+void printList(LinkedList list) {
+    Node* temp = list.head;
     while (temp != NULL) {
         printf("%d ", temp->data);
         temp = temp->next;
--- Data_Structures/Linked-List/C/linkedList.h
+++ Data_Structures/Linked-List/C/linkedList.h
@@ -1,8 +1,20 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
-struct Node* newNode(int data);
-void connectNode(struct Node* node, struct Node* nextNode);
-void printList(struct Node* head);
+// Definição da estrutura do nó
+typedef struct {
+    int data;
+    struct Node* next;} 
+    Node;
+
+// Definição da estrutura da lista encadeada
+typedef struct {
+       struct Node* head;
+} LinkedList;
+
+Node newNode(int data);
+LinkedList newLinkedList();
+void connectNode(Node node, Node nextNode);
+void printList(LinkedList list);
 
 #endif // LINKEDLIST_H
