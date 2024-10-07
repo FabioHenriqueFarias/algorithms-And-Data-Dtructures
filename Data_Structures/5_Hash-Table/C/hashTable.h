6f15c91197a80b7f31de9249c07be34553ecff9a
FabioFarias <fabioh.farias@outlook.com.br>
2024-10-07 16:15:55 -0300

    update Hash Table

--- Data_Structures/5_Hash-Table/C/hashTable.h
+++ Data_Structures/5_Hash-Table/C/hashTable.h
@@ -1,12 +1,26 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 
-// Estrutura para a pilha
-typedef struct HashTable{
-    int MAX = 100;
-    int *arr;   
-    int *list;
-      
-} TabelaHash;
+#include <stdlib.h>
+
+// Estrutura para um item na tabela hash
+typedef struct Ht_item {
+    char* key;      // Chave do item
+    char* value;    // Valor do item
+} Ht_item;
+
+// Estrutura da tabela hash
+typedef struct HashTable {
+    Ht_item** items; // Array de ponteiros para itens
+    int size;        // Tamanho da tabela hash
+    int count;       // Contador de itens armazenados
+} HashTable;
+
+// Funções para manipulação da tabela hash
+HashTable* inicializarTabelaHash(int size);
+void insert(HashTable* ht, const char* key, const char* value);
+char* search(HashTable* ht, const char* key);
+void delete(HashTable* ht, const char* key);
+void freeTable(HashTable* ht);
 
 #endif // HASHTABLE_H
