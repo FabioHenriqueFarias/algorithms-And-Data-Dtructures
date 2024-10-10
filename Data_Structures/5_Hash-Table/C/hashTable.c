713e11d19789a8a1e370198117d525be844f4866
FabioFarias <fabioh.farias@outlook.com.br>
2024-10-10 10:43:16 -0300

    update Hash Table

--- Data_Structures/5_Hash-Table/C/hashTable.c
+++ Data_Structures/5_Hash-Table/C/hashTable.c
@@ -10,8 +10,14 @@ HashTable* inicializarTabelaHash(int size) {
         printf("Erro: Não foi possível alocar memória para a nova tabela hash.\n");
         exit(1);
     }
-    ht -> size = size;
 
+    ht -> array = (Node**)malloc(size * sizeof(Node*));
+    if (ht->array == NULL) {
+        printf("Erro: Não foi possível alocar memória para o array de listas encadeadas.\n");
+        exit(1);
+    }
+
+    ht->size = size;
     return ht;
 }
 
@@ -54,8 +60,39 @@ void insert(HashTable* ht, const char* key, const char* value) {
     }
 }
 
+// Função para buscar um item na tabela hash
+char* search(HashTable* ht, const char* key) {
+    unsigned long index = getHash(ht, key);
+    Node* current = ht->array[index]; // Pega o primeiro nó da lista encadeada
+
+    // Itera sobre a lista encadeada
+    while (current != NULL) {
+        if (strcmp(current->key, key) == 0) {
+            return current->value; // Retorna o valor se a chave for encontrada
+        }
+        current = current->next; // Avança para o próximo nó
+    }
+
+    return NULL; // Retorna NULL se a chave não for encontrada
+}
+
 // Função para liberar a memória da tabela hash
 void freeTable(HashTable* ht) {
     if (ht == NULL) return; // Verifica se a tabela hash é NULL
+
+    // Itera sobre cada índice do array
+    for (int i = 0; i < ht->size; i++) {
+        Node* current = ht->array[i]; // Pega o primeiro nó da lista encadeada
+        while (current != NULL) {
+            Node* temp = current; // Armazena o nó atual
+            current = current->next; // Avança para o próximo nó
+            free(temp->key); // Libera a memória da chave
+            free(temp->value); // Libera a memória do valor
+            free(temp); // Libera a memória do nó
+        }
+    }
+
+    free(ht->array); // Libera a memória do array
+    free(ht); // Libera a memória da tabela hash em si
   
 }
